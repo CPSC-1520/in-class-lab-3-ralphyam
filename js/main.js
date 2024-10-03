85302ba407e185932e3f1be513e0d83048837bde
ralphyam <ryammine1@nait.ca>
2024-10-02 21:13:00 -0600

    more testing
    testing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 // Enter your code below.
+const resourcesDiv = document.querySelector('.javascript-resources');
+const showButton = document.getElementById('show-resources');
+showButton.addEventListener('click', () => {
+resourcesDiv.classList.remove('d-none');});
+const listItems = document.querySelectorAll('.javascript-resources .list-group-item');
+listItems.forEach(item => {
+  item.addEventListener('mouseover', (event) => {
+    console.log(event.target);
+    event.target.classList.add('fw-bold');
+  });
+  item.addEventListener('mouseout', (event) => {
+    event.target.classList.remove('fw-bold');
+  });
+});
