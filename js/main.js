a3b583705bb1e8e05bccddb4fda9e0a036f02ca5
ralphyam <ryammine1@nait.ca>
2024-10-02 21:16:43 -0600

    format
    fixed formatting

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 // Enter your code below.
 const resourcesDiv = document.querySelector('.javascript-resources');
 const showButton = document.getElementById('show-resources');
-showButton.addEventListener('click', () => {
-resourcesDiv.classList.remove('d-none');});
+showButton.addEventListener('click', () => {resourcesDiv.classList.remove('d-none');});
 const listItems = document.querySelectorAll('.javascript-resources .list-group-item');
 listItems.forEach(item => {
   item.addEventListener('mouseover', (event) => {
